9df94af971fa9475610748bff2d1b9850e6dfd04
fatouuuu <fatmaabdullrahim@gmail.com>
2022-11-29 12:16:32 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // code your solution here
+
+
+function superbowlWin(record){
+    let findWin =  record.find(isAWin)
+    if(findWin == null) {
+        return undefined;
+    } else {
+        return findWin.year
+    }    
+    
+
+}
+
+function isAWin(record) {
+  return record.result=="W"
+}
+
+
+
+// function isAWin(record){
+//     return record.result === 'W'
+// }
+
+// function superbowlWin(record){
+//      let findWin = record.find(isAWin)
+//     if  (findWin == null) {
+//         return record.year
+//     }
+//     else {
+//         return undefined
+//     }
+// }
+    
+// const findWin = record.find((record)=>{
+//         if (record.result = 'W'){
+//             return record.year
+//         }
+// })
